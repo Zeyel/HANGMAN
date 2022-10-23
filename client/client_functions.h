fc2aa05d041428c327d188b1a7eb835c0255fedf
Zeyel <tim.branstett@gmail.com>
2022-10-23 16:19:42 +0200

    includes

--- client/client_functions.h
+++ client/client_functions.h
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys.socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 /////////////////////////////
 
 /*TYPES*/
