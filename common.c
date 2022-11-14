f4d73e2cf7b4ab4f79e950d3ab72335a393d1a5f
Lyner-Eoria <vflorent@orange.fr>
2022-11-14 18:47:39 +0100

    Common
    Co-authored-by: Tim <Zeyel@users.noreply.github.com>

---
+++ common.c
@@ -0,0 +1,6 @@
+#include "common.h"
+
+void init_options(options_t *options) {
+    options->name = malloc(100);
+    options->list = malloc(100);
+}
--- common.h
+++ common.h
@@ -11,11 +11,38 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-/*DEFINE SOCKETS*/
+#define MSG_QUIT       -1
+#define MSG_START_GAME 1
+#define MSG_CHEAT_CODE 666
+#define MSG_OPTIONS    2
+#define MSG_ADD        3
+#define MSG_LETTER     4
+#define MSG_WORD       5
+
+#define MSG_SIZE 128
+
 #define CONNECT_PORT 50000
 #define IPV4         PF_INET
 #define TCP          SOCK_STREAM
 #define PROTOCOL     0
 
-/*VARIABLES*/
+typedef enum {
+    vs_server = 1,
+    vs_player,
+    player_vs_player
+} game_type_t;
+
+typedef struct {
+    char *name;
+    int tries;
+    int min;
+    int max;
+    int state;
+    char *list;
+    int time;
+    game_type_t type;
+} options_t;
+
+void init_options(options_t *options);
+
 #endif
